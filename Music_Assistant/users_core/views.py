f20783e027e5412cdd8feeb3a03971428d96d025
Ivanov_Anton <anton-ivanov-080203@mail.ru>
2024-01-04 00:09:36 +0300

    Теперь оптимально работают кнопки Добавить и Удалить

--- Music_Assistant/users_core/views.py
+++ Music_Assistant/users_core/views.py
@@ -189,16 +189,17 @@ def form_aut_sending(request):
 # и при повторном заходе мы получаем error - чтение пустых данных
 # так что, пока пусть будет так
 
+# ЗАГРУЗКА ДАННЫХ В ОБЩУЮ БИБЛИОТЕКУ НА СТРАНИЦЕ
 all_music = []
-field_names = [field.name for field in MusicBase._meta.fields][2:]
+# field_names = [field.name for field in MusicBase._meta.fields][2:]
 music_objects = MusicBase.objects.all()
-table_data = []
+# table_data = []
 for obj in music_objects:
-    row_data = [getattr(obj, field) for field in field_names]
-    music_id = obj.dop_id
-    add_button = f'<button class="dop-button" data-id="{music_id}">Добавить</button>'
-    row_data.append(add_button)
-    table_data.append(row_data)
+    # row_data = [getattr(obj, field) for field in field_names]
+    # music_id = obj.dop_id
+    # add_button = f'<button class="dop-button" data-id="{music_id}">Добавить</button>'
+    # row_data.append(add_button)
+    # table_data.append(row_data)
     all_music.append({"dop_id": obj.dop_id, "genre": obj.genre, "author": obj.author,
                       "co_author": obj.co_author, "album": obj.album, "title": obj.title})
 def main(request):
@@ -258,28 +259,46 @@ def main(request):
         </div>
                 '''
 
-    # загрузка данных в личную библиотеку на странице
+    # ЗАГРУЗКА ДАННЫХ В ЛИЧНУЮ БИБЛИОТЕКУ НА СТРАНИЦЕ
     user_coll = request.COOKIES.get('user_collection')
     user = Users.objects.get(nickname=user_coll)
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client[settings.DATABASES['links_mongodb']['NAME']]
-    # получаем список всех коллекций в базе данных
-    collection_names = db.list_collection_names()
-    # проверяем, есть ли нужная коллекция
-    for collection_name in collection_names:
-        if collection_name == user_coll:
-            user_collection = db.get_collection(collection_name)
-            user_doc = user_collection.find_one({"email": user.email})
-            music_entries = user_doc["links"] if "links" in user_doc else []
-            field_order = ['genre', 'author', 'co_author', 'album', 'title']
-            table_data_2 = []
-            for entry in music_entries:
-                # тут код обработки данных отличается, так как в mongodb всё хранится в виде словарей
-                row_data_2 = [entry.get(field, "") for field in field_order]  # используем obj[field] или obj.get(field) (словарный доступ)
-                music_id_2 = entry.get('dop_id')  # используем словарный доступ к 'dop_id'
-                del_button = f'<button class="del-button" data-id="{music_id_2}">Удалить</button>'
-                row_data_2.append(del_button)
-                table_data_2.append(row_data_2)
+    with MongoClient(settings.DATABASES['links_mongodb']['CLIENT']['host']) as client:
+        db = client[settings.DATABASES['links_mongodb']['NAME']]
+        user_collection = db[user_coll]
+        user_doc = user_collection.find_one({"email": user.email})
+        music_entries = user_doc["links"] if "links" in user_doc else []
+        field_order = ['genre', 'author', 'co_author', 'album', 'title']
+        table_data_2 = []
+        for entry in music_entries:
+            # тут код обработки данных отличается, так как в mongodb всё хранится в виде словарей
+            row_data_2 = [entry.get(field, "") for field in field_order]  # используем obj[field] или obj.get(field) (словарный доступ)
+            music_id_2 = entry.get('dop_id')  # используем словарный доступ к 'dop_id'
+            del_button = f'<button class="del-button" data-id="{music_id_2}">Удалить</button>'
+            row_data_2.append(del_button)
+            table_data_2.append(row_data_2)
+
+        # создаем set из Music ID личной коллекции для ускорения проверки вхождения
+        user_music_ids = set(entry['dop_id'] for entry in music_entries)
+
+    # ЗАГРУЗКА ДАННЫХ В ОБЩУЮ БИБЛИОТЕКУ НА СТРАНИЦЕ
+    with MongoClient(settings.DATABASES['music_mongodb']['CLIENT']['host']) as client:
+        db = client[settings.DATABASES['music_mongodb']['NAME']]
+        user_collection = db['music_base']
+        user_doc = user_collection.find()
+        field_names = ["genre", "author", "co_author", "album", "title"]
+        table_data = []
+        for record in user_doc:
+            row_data = [record.get(field, "") for field in field_names]
+            music_id = record.get("dop_id")
+            # проверяем, существует ли запись в личной коллекции пользователя
+            if music_id in user_music_ids:
+                # если запись существует, кнопка невидимая
+                add_button = f'<button class="dop-button" style="display: none;" data-id="{music_id}">Добавить</button>'
+            else:
+                # если записи нет, кнопка видимая
+                add_button = f'<button class="dop-button" data-id="{music_id}">Добавить</button>'
+            row_data.append(add_button)
+            table_data.append(row_data)
 
     data = {"links": links, "scripts": scripts, "title": title, "videos": videos, "all_data_1": all_data_1,
             "all_data_2": all_data_2, "field_data": field_names, "table_data": table_data, "all_data_3": all_data_3,
@@ -318,91 +337,8 @@ def save_to_library(request):
                 {"$push": {"links": {"$each": save_music}}}  # используем $each для добавления всех записей
             )
             save_music.clear()
-
-        # загрузка статичных объектов
-        links = [static("css/main.css")]
-        scripts = [static("js/main.js")]
-        title = "Рабочая зона"
-        videos = [static("video/equalizer.mp4")]
-        all_data_1 = '''
-                    <div class="content">
-                        <div class="container">
-                    '''
-        all_data_2 = '''
-                    <div class="horizontal-orientation">
-                        <div class="adaptive">
-                            <h4>Музыкальная база</h4>
-                            <div class="whole-library" id="wholeLibrary">
-                                <div class="whole-library-textarea" style="display: none;" id="wholeLibraryTextarea">
-                    '''
-        all_data_3 = '''
-                                </div>
-                                <button class="button-popup button-slide slide-inside" type="button" id="openWholeLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeWholeLibrary">ЗАКРЫТЬ</button>
-                    '''
-        all_data_4 = '''
-                            </div>
-                        </div>
-                        <div class="adaptive">
-                            <h4>Моя библиотека</h4>
-                            <div class="my-library" id="myLibrary">
-                                <div class="whole-library-textarea" style="display: none;" id="myLibraryTextarea">
-                        '''
-        all_data_5 = '''
-                                </div>
-                                <button class="button-popup button-slide slide-inside" type="button" id="openMyLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeMyLibrary">ЗАКРЫТЬ</button>
-                        '''
-        all_data_6 = '''
-                            </div>
-                        </div>
-                        <div class="adaptive">
-                            <h4>Узнать рекомендации</h4>
-                            <div class="recomendation-library" id="recomendationLibrary">
-                                <button class="button-popup button-slide slide-inside" type="button" id="openRecomendationLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeRecomendationLibrary">ЗАКРЫТЬ</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                    '''
-
-        # загрузка данных в личную библиотеку на странице
-        user_coll = request.COOKIES.get('user_collection')
-        user = Users.objects.get(nickname=user_coll)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client[settings.DATABASES['links_mongodb']['NAME']]
-        # получаем список всех коллекций в базе данных
-        collection_names = db.list_collection_names()
-        # проверяем, есть ли нужная коллекция
-        for collection_name in collection_names:
-            if collection_name == user_coll:
-                user_collection = db.get_collection(collection_name)
-                user_doc = user_collection.find_one({"email": user.email})
-                music_entries = user_doc["links"] if "links" in user_doc else []
-                field_order = ['genre', 'author', 'co_author', 'album', 'title']
-                table_data_2 = []
-                for entry in music_entries:
-                    # тут код обработки данных отличается, так как в mongodb всё хранится в виде словарей
-                    row_data_2 = [entry.get(field, "") for field in
-                                  field_order]  # используем obj[field] или obj.get(field) (словарный доступ)
-                    music_id_2 = entry.get('dop_id')  # используем словарный доступ к 'dop_id'
-                    del_button = f'<button class="del-button" data-id="{music_id_2}">Удалить</button>'
-                    row_data_2.append(del_button)
-                    table_data_2.append(row_data_2)
-
-        data = {"links": links, "scripts": scripts, "title": title, "videos": videos, "all_data_1": all_data_1,
-                "all_data_2": all_data_2, "field_data": field_names, "table_data": table_data, "all_data_3": all_data_3,
-                "all_data_4": all_data_4, "field_data_2": field_order, "table_data_2": table_data_2,
-                "all_data_5": all_data_5, "all_data_6": all_data_6}
-        return render(request, "main.html", context=data)
+        # чтобы не дублировать код переадресации и вывода данных - просто вызываем функцию
+        return main(request)
     else:
         return HttpResponseBadRequest("Разрешены только POST запросы!")
 
@@ -438,90 +374,7 @@ def save_delete_from_library(request):
                 {"$pull": {"links": {"dop_id": {"$in": music_ids_to_remove}}}}  # применяем $pull операцию с использованием $in
             )
             del_music.clear()  # очистить массив после удаления
-
-        # загрузка статичных объектов
-        links = [static("css/main.css")]
-        scripts = [static("js/main.js")]
-        title = "Рабочая зона"
-        videos = [static("video/equalizer.mp4")]
-        all_data_1 = '''
-                    <div class="content">
-                        <div class="container">
-                    '''
-        all_data_2 = '''
-                    <div class="horizontal-orientation">
-                        <div class="adaptive">
-                            <h4>Музыкальная база</h4>
-                            <div class="whole-library" id="wholeLibrary">
-                                <div class="whole-library-textarea" style="display: none;" id="wholeLibraryTextarea">
-                    '''
-        all_data_3 = '''
-                                </div>
-                                <button class="button-popup button-slide slide-inside" type="button" id="openWholeLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeWholeLibrary">ЗАКРЫТЬ</button>
-                    '''
-        all_data_4 = '''
-                            </div>
-                        </div>
-                        <div class="adaptive">
-                            <h4>Моя библиотека</h4>
-                            <div class="my-library" id="myLibrary">
-                                <div class="whole-library-textarea" style="display: none;" id="myLibraryTextarea">
-                        '''
-        all_data_5 = '''
-                                </div>
-                                <button class="button-popup button-slide slide-inside" type="button" id="openMyLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeMyLibrary">ЗАКРЫТЬ</button>
-                        '''
-        all_data_6 = '''
-                            </div>
-                        </div>
-                        <div class="adaptive">
-                            <h4>Узнать рекомендации</h4>
-                            <div class="recomendation-library" id="recomendationLibrary">
-                                <button class="button-popup button-slide slide-inside" type="button" id="openRecomendationLibrary">ПОКАЗАТЬ</button>
-                            </div>
-                            <div class="button-center">
-                                <button class="button-popup button-slide slide-inside" style="display: none;" type="button" id="closeRecomendationLibrary">ЗАКРЫТЬ</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                    '''
-
-        # загрузка данных в личную библиотеку на странице
-        user_coll = request.COOKIES.get('user_collection')
-        user = Users.objects.get(nickname=user_coll)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client[settings.DATABASES['links_mongodb']['NAME']]
-        # получаем список всех коллекций в базе данных
-        collection_names = db.list_collection_names()
-        # проверяем, есть ли нужная коллекция
-        for collection_name in collection_names:
-            if collection_name == user_coll:
-                user_collection = db.get_collection(collection_name)
-                user_doc = user_collection.find_one({"email": user.email})
-                music_entries = user_doc["links"] if "links" in user_doc else []
-                field_order = ['genre', 'author', 'co_author', 'album', 'title']
-                table_data_2 = []
-                for entry in music_entries:
-                    # тут код обработки данных отличается, так как в mongodb всё хранится в виде словарей
-                    row_data_2 = [entry.get(field, "") for field in
-                                  field_order]  # используем obj[field] или obj.get(field) (словарный доступ)
-                    music_id_2 = entry.get('dop_id')  # используем словарный доступ к 'dop_id'
-                    del_button = f'<button class="del-button" data-id="{music_id_2}">Удалить</button>'
-                    row_data_2.append(del_button)
-                    table_data_2.append(row_data_2)
-
-        data = {"links": links, "scripts": scripts, "title": title, "videos": videos, "all_data_1": all_data_1,
-                "all_data_2": all_data_2, "field_data": field_names, "table_data": table_data, "all_data_3": all_data_3,
-                "all_data_4": all_data_4, "field_data_2": field_order, "table_data_2": table_data_2,
-                "all_data_5": all_data_5, "all_data_6": all_data_6}
-        return render(request, "main.html", context=data)
+        # чтобы не дублировать код переадресации и вывода данных - просто вызываем функцию
+        return main(request)
     else:
         return HttpResponseBadRequest("Разрешены только POST запросы!")
